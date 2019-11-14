8fe11aec6f21b1433c36872bcb41f1a4c4eefc88
Jay <baileyjarron@gmail.com>
2019-11-13 22:13:04 -0600

    assignment 6

---
+++ assignment6/Controllers/MortgageController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace assignment7.Controllers
+{
+    public class MortgageController : Controller
+    {
+        // GET: Mortgage
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        // POST: Mortgage
+        [HttpPost]
+        public ActionResult Index(CalculationModel calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                //double principle, double years, double rate
+                calculation.monthlyPayment = MortgageHelper.useComputeMonthlyPayment(calculation.principle, calculation.years, calculation.rate);
+                return View("PaymentDetails", calculation);
+            } else
+            {
+                return View("Index", calculation);
+            }
+        }
+
+        // GET: Edit
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View("Edit");
+        }
+
+        // POST: Edit
+        [HttpPost]
+        public ActionResult Edit(CalculationModel calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                //double principle, double years, double rate
+                calculation.monthlyPayment = MortgageHelper.useComputeMonthlyPayment(calculation.principle, calculation.years, calculation.rate);
+                return View("PaymentDetails", calculation);
+            }
+            else
+            {
+                return View("Index", calculation);
+            }
+        }
+    }
+}
---
+++ assignment6/obj/Models/CalculationModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace assignment7
+{
+    [Bind(Include = "principle, years, rate", Exclude = "monthlyPayment")]
+    public class CalculationModel
+    {
+        public CalculationModel(double principal, double rate, double years, double monthlyPayment)
+        {
+            this.principle = principal;
+            this.rate = rate;
+            this.years = years;
+            this.monthlyPayment = monthlyPayment;
+        }
+
+        public CalculationModel()
+        {
+         
+        }
+
+        [DisplayName("Principle")]
+        [Required]
+        public double principle { get; set; }
+        [DisplayName("Years")]
+        [Required]
+        [Range(0, 100)]
+        public double years { get; set; }
+        [DisplayName("Interest Rate")]
+        [Required]
+        [Range(0, 10)]
+        public double rate { get; set; }
+        [DataType(DataType.Currency)]
+        public double monthlyPayment { get; set; }
+
+        public override string ToString()
+        {
+            return $"{principle};{rate};{years};{monthlyPayment}";
+        }
+    }
+}
